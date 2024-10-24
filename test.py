1667f1d4bfaebac82c6e926c54bf2e4cc1abd77b
Jonathan Boyd <31322519+JonathanBoyd96@users.noreply.github.com>
2024-10-24 11:31:42 -0400

    Revert "update"
    This reverts commit 385189b5fa75b2bc6e6bf259462b7ba2c3a3c7c4.

--- test.py
+++
@@ -1,7 +0,0 @@
-import spacy
-
-# Load a pre-trained model
-nlp = spacy.load("model/data_model")
-doc = nlp("Erik Adams, 555 Birch Road Suite 404, Columbus, OH 43215")
-for ent in doc.ents:
-    print(ent.text, ent.label_)
